ef51a3e934a5b3ca4efa4019ae0e2e5462e5c5a7
ghazanfarali1994 <ghazanfar006@gmail.com>
2022-08-21 12:57:50 +0500

    testing clarifai

--- src/App.js
+++ src/App.js
@@ -7,14 +7,32 @@ import Particle from './components/Particle/Particle';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+
+  onButtonSubmit = () => {
+    console.log('click');
+  }
+
   render() {
     return (
       <div className='App'>
-        <Particle />
+        <Particle className='particle' />
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
+        <ImageLinkForm 
+           onInputChange={this.onInputChange} 
+           onButtonSubmit={this.onButtonSubmit}
+        />
         {/* <FaceRecognition /> */}
       </div>
     );
